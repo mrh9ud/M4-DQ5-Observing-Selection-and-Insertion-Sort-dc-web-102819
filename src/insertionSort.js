12e9cb9a62aa5836da16fa7bc668890264443ba3
mrh9ud <mrh9ud@virginia.edu>
2020-01-13 09:46:50 -0500

    Done.

--- src/insertionSort.js
+++ src/insertionSort.js
@@ -1,4 +1,16 @@
 function insertionSort(arr) {
   // your code here!
-  // arr is an array of unsorted integers (i.e. [3, 5, 1])
+  // arr is an array of unsorted integers (i.e. [3, 5, 1]) {
+    const length = arr.length;
+  
+    for (let i = 1; i < length; i++) {
+        let key = arr[i];
+        let j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j+1] = key;
+    }
+    return arr;
 }
--- src/selectionSort.js
+++ src/selectionSort.js
@@ -1,4 +1,26 @@
+array = [5, 4, 2, 6]
+
 function selectionSort(arr) {
-  // your code here!
-  // arr is an array of unsorted integers (i.e. [3, 5, 1])
+  let i,j
+
+  for(i = 0; i < arr.length-1; i++){
+    let  jMin = i
+    for(j = i+1; j< arr.length; j++){
+      if(arr[j] < arr[jMin]){
+        jMin = j
+      }
+    }
+    if(jMin != i)
+    {
+      swap(arr, i, jMin)
+    }
+  }
+  console.log(arr)
 }
+function swap(A, x, y) {
+  let temp = A[x]
+  A[x] = A[y]
+  A[y] = temp
+  // return [ y , x]
+}
+console.log(selectionSort(array))
